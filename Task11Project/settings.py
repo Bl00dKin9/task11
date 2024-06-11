09347603b32c734ee7777037a96bc326060c222a
Bl00dKin9 <realbloodking@gmail.com>
2024-06-11 18:03:10 +0700

    Создал подключение к внешней базе данных

--- Task11Project/settings.py
+++ Task11Project/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Task11Project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hakaton',
+        'USER': 'root',
+        'PASSWORD': 'Wargateqwert4',
+        'HOST': '46.243.227.44',
+        'PORT': '5432',
     }
 }
 
