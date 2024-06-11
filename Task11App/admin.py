ae78bd054a474a6b384fd99fef1ffea9cd4a07b9
Bl00dKin9 <realbloodking@gmail.com>
2024-06-11 15:56:54 +0700

    Создал модели и зарегистрировал их

--- Task11App/admin.py
+++ Task11App/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class BuildingAdmin(admin.ModelAdmin):
+    pass
+
+
+class ContractAdmin(admin.ModelAdmin):
+    pass
+
+
+class ContractBuildingConnectionAdmin(admin.ModelAdmin):
+    pass
+
+
+class FixedAssetAdmin(admin.ModelAdmin):
+    pass
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    pass
+
+
+class InvoiceForPaymentAdmin(admin.ModelAdmin):
+    pass
+
+
+class DistributedInvoiceForPaymentAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Building, BuildingAdmin)
+admin.site.register(Contract, ContractAdmin)
+admin.site.register(ContractBuildingConnection, ContractBuildingConnectionAdmin)
+admin.site.register(FixedAsset, FixedAssetAdmin)
+admin.site.register(Service, ServiceAdmin)
+admin.site.register(InvoiceForPayment, InvoiceForPaymentAdmin)
+admin.site.register(DistributedInvoiceForPayment, DistributedInvoiceForPaymentAdmin)
